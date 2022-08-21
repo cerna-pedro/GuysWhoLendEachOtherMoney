b9a2232a557f9acc6c34d89f135327c602d865ca
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-08-21 13:00:35 -0500

    Finalized console app.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-           
+            Guy joe = new Guy() { Cash = 50, Name = "Joe" };
+            Guy bob = new Guy() { Name = "Bob", Cash = 100 };
+
+            while (true)
+            {
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
+
+                Console.Write("Enter an amount: ");
+                string howMuch = Console.ReadLine();
+                if (howMuch == "") return;
+                if(int.TryParse(howMuch, out int amount))
+                {
+                    Console.Write("Who should give the cash: ");
+                    string whichGuy = Console.ReadLine();
+                    if (whichGuy == "Joe" || whichGuy == "joe" || whichGuy == "JOE")
+                    {
+                        bob.ReceiveCash(joe.GiveCash(amount));
+                    }
+                    else if (whichGuy == "Bob" || whichGuy == "bob" || whichGuy == "BOB")
+                    {
+                        joe.ReceiveCash(bob.GiveCash(amount));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter 'Joe' or 'Bob'");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount (or a blank line to exit).");
+                }
+            }
+
         }
     }
 }
